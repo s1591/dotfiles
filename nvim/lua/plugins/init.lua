3a5b6d1df097878c78295936a76178ee9dd3d30a
s1591 <sumanthpavuluri1591@gmail.com>
2024-06-03 13:53:26 +0530

    -_

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -87,7 +87,7 @@ return {
                     python = { "cd $dir && python $fileName" },
                     cpp = { "cd $dir && clang++ -std=c++20 $fileName && ./a.out && rm ./a.out" },
                     c = { "cd $dir && gcc $fileName && ./a.out && rm ./a.out" },
-                    --go = { "cd $dir && go run ." },
+                    go = { "cd $dir && go run ." },
                     --rust = { "cd $dir && rustc $fileName && ./$fileNameWithoutExt && rm ./$fileNameWithoutExt" },
                 },
             }
