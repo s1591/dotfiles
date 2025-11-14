a08efec802cb4b265f3b1ff29f792ac2c4553d0d
s1591 <sumanthpavuluri1591@gmail.com>
2025-11-14 22:17:03 +0530

    -_

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "gopls", "rust_analyzer", "clangd", "pyright", "html", "jdtls", "csharp_ls" }
+local servers = { "gopls", "rust_analyzer", "clangd", "pyright", "html", "jdtls", "csharp_ls", "ts_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
