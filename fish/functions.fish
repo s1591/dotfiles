e43ae23b54cc266a788d5d980e52c2f07cf82849
s1591 <sumanthpavuluri1591@gmail.com>
2026-03-19 17:10:12 +0530

    -_

--- fish/functions.fish
+++ fish/functions.fish
@@ -258,3 +258,12 @@ function notify
     end
 
 end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
